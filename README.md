833a96a8e4901186afcd7b6f5525457c90cf6c34
Florian Bruhin <me@the-compiler.org>
2020-09-07 21:57:48 +0200

    Add data

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # nasebohr
+
 A command-line tool for DNS queries, with colors and beautiful output
+
+![screenshot](.github/img/screenshot.png)
+
+## FAQ
+
+### What does the name mean?
+
+The German "nasebohr" literally means "nose-bore", but loosely translates to
+"picking one's nose". Given that existing DNS client utilities are named
+[dig](https://en.wikipedia.org/wiki/Dig_(command)) and
+[drill](https://linux.die.net/man/1/drill), "bohr" seems like a logical
+evolution. Given that we're looking at **na**me **se**rvers, the "nase" is more
+than fitting as well.
---
+++ nasebohr.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+# Copyright 2020 Florian Bruhin (The Compiler) <me@the-compiler.org>
+# All rights reserved. Don't worry, I'll upload this to GitHub under a free
+# license ASAP. I just need some food now.
+
+import argparse
+
+import rich
+import dns.resolver
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('name', help='Name or names, separated by comma')
+    parser.add_argument('rrtype', nargs='?', default='A',
+                        help='RR type or types, separated by comma')
+    parser.add_argument('server', nargs='?',
+                        help='Nameserver to query')
+    return parser.parse_args()
+
+
+def do_query(resolver, name, rrtype):
+    try:
+        answer = resolver.query(name, rrtype)
+    except dns.exception.DNSException as e:
+        rich.print(f'[red]{e}[/red]')
+        return
+
+    for rr in answer:
+        rich.print(f'[white]{name}[/white] [bright_black]{rrtype:3}[/bright_black] {rr}')
+
+
+def main():
+    args = parse_args()
+
+    resolver = dns.resolver.Resolver(configure=args.server is None)
+    if args.server is not None:
+        resolver.nameservers = [args.server.lstrip('@')]
+
+    for name in args.name.split(','):
+        for rrtype in args.rrtype.split(','):
+            do_query(resolver, name, rrtype)
+
+
+if __name__ == '__main__':
+    main()
